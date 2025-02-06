11982748abef85f05df4910d98d7a53972267399
sol-garcia <sol.garcia@threatx.com>
2025-02-06 11:02:35 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   images: {
     unoptimized: true,
   },
   experimental: {
-    serverActions: true
+    serverActions: {},
   }
 };
 
